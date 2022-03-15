dbd95f9f5fe40e2d03968f29c7edaa9039bde3d8
Yevgeniy Wyszyński <yevgeniywyszynski@Yevgeniys-MacBook-Pro.local>
2022-03-15 21:47:36 +0100

    Add handling of the first form and the join button

--- client/app.js
+++ client/app.js
@@ -1,8 +1,21 @@
 const loginForm = document.getElementById('welcome-form');
-const messagesSection = document.getElementById('messeges-section');
+const messagesSection = document.getElementById('messages-section');
 const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 
 let userName = []
+
+/* 1 form */
+
+loginForm.addEventListener('submit', function login(event) {
+    event.preventDefault()
+    if(!userNameInput.value) {
+        alert('wpisz nazwę uytkownika')
+    } else {
+        userName = userNameInput.value;
+        loginForm.classList.remove('show')
+        messagesSection.classList.add('show')
+    }
+})
