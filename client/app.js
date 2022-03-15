7d2dce4f599bbaf5faae4509af58a5492ed5c5d5
Yevgeniy Wyszyński <yevgeniywyszynski@Yevgeniys-MacBook-Pro.local>
2022-03-15 21:39:32 +0100

    Add index.html, style.css, app.js

---
+++ client/app.js
@@ -0,0 +1,8 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messeges-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName = []
